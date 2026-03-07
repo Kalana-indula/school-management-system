755b14bc9051e96a5e9fc6d8164794b40d3e45a7
kalan <indulakalana@gmail.com>
2026-03-07 13:20:54 +0530

    charts card added

--- front-end/app/(dashboard)/admin/page.tsx
+++ front-end/app/(dashboard)/admin/page.tsx
@@ -1,8 +1,40 @@
 import React from 'react'
+import UserCard from "@/app/components/UserCard";
+import CountChart from "@/app/components/CountChart";
 
 const AdminPage = () => {
     return (
-        <div>AdminPage</div>
+        <div className="p-4 flex gap-4 flex-col md:flex-row">
+        {/*  LEFT  */}
+            <div className="w-full lg:w-2/3 flex flex-col gap-8">
+            {/*  USER CARD  */}
+                <div className="flex gap-4 justify-between flex-wrap">
+                    <UserCard type="student"/>
+                    <UserCard type="teacher"/>
+                    <UserCard type="parent"/>
+                    <UserCard type="staff"/>
+                </div>
+            {/*  MIDDLE CHARTS  */}
+                <div>
+                {/*  COUNT CHART  */}
+                    <div className="w-full lg:w-1/3 h-[450px]">
+                        <CountChart/>
+                    </div>
+                {/*  ATTENDANCE CHART  */}
+                    <div className="w-full lg:w-2/3 h-[450px]">
+
+                    </div>
+                </div>
+            {/*  BOTTOM CHART  */}
+                <div>
+
+                </div>
+            </div>
+        {/*  RIGHT  */}
+            <div className="w-full lg:w-1/3">
+                r
+            </div>
+        </div>
     )
 }
 export default AdminPage
